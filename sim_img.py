76607c53584d1c3f1842d263d0a0a28d67bb7d8a
sunzy301 <sunzy301@aliyun.com>
2018-05-28 15:27:43 +0800

    create sim_img.py

---
+++ sim_img.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+import skimage.io
+import skimage.feature
+import skimage.color
+import skimage.transform
+import skimage.util
+import skimage.segmentation
+import numpy
+
+
+# "Selective Search for Object Recognition" by J.R.R. Uijlings et al.
+#
+#  - Modified version with LBP extractor for texture vectorization
+
+def _sim_colour(r1, r2):
+    """
+        calculate the sum of histogram intersection of colour
+    """
+    return sum([min(a, b) for a, b in zip(r1["hist_c"], r2["hist_c"])])
+
+
+def _sim_texture(r1, r2):
+    """
+        calculate the sum of histogram intersection of texture
+    """
+    return sum([min(a, b) for a, b in zip(r1["hist_t"], r2["hist_t"])])
+
+def _calc_colour_hist(img):
+    """
+        calculate colour histogram for each region
+
+        the size of output histogram will be BINS * COLOUR_CHANNELS(3)
+
+        number of bins is 25 as same as [uijlings_ijcv2013_draft.pdf]
+
+        extract HSV
+    """
+
+    BINS = 25
+    hist = numpy.array([])
+
+    for colour_channel in (0, 1, 2):
+
+        # extracting one colour channel
+        c = img[:, colour_channel]
+
+        # calculate histogram for each colour and join to the result
+        hist = numpy.concatenate(
+            [hist] + [numpy.histogram(c, BINS, (0.0, 255.0))[0]])
+
+    # L1 normalize
+    hist = hist / len(img)
+
+    return hist
+
+
+def _calc_texture_gradient(img):
+    """
+        calculate texture gradient for entire image
+
+        The original SelectiveSearch algorithm proposed Gaussian derivative
+        for 8 orientations, but we use LBP instead.
+
+        output will be [height(*)][width(*)]
+    """
+    ret = numpy.zeros((img.shape[0], img.shape[1], img.shape[2]))
+
+    for colour_channel in (0, 1, 2):
+        ret[:, :, colour_channel] = skimage.feature.local_binary_pattern(
+            img[:, :, colour_channel], 8, 1.0)
+
+    return ret
+
+
+def _calc_texture_hist(img):
+    """
+        calculate texture histogram for each region
+
+        calculate the histogram of gradient for each colours
+        the size of output histogram will be
+            BINS * ORIENTATIONS * COLOUR_CHANNELS(3)
+    """
+    BINS = 10
+
+    hist = numpy.array([])
+
+    for colour_channel in (0, 1, 2):
+
+        # mask by the colour channel
+        fd = img[:, colour_channel]
+
+        # calculate histogram for each orientation and concatenate them all
+        # and join to the result
+        hist = numpy.concatenate(
+            [hist] + [numpy.histogram(fd, BINS, (0.0, 1.0))[0]])
+
+    # L1 Normalize
+    hist = hist / len(img)
+
+    return hist
+
+def _generate_hist(img):
+    R = {}
+
+    # get hsv image
+    hsv = skimage.color.rgb2hsv(img[:, :, :3])
+
+    # pass 2: calculate texture gradient
+    tex_grad = _calc_texture_gradient(img)
+
+    # pass 3: calculate  of each region
+    # colour histogram
+    R["hist_c"] = _calc_colour_hist(img)
+
+    # texture histogram
+    R["hist_t"] = _calc_texture_hist(tex_grad)
+
+    return R
+
+def get_feature(img):
+    r1 = _generate_hist(img)
+    return r1
+
+def calc_sim(r1, r2):
+    # r1 = _generate_hist(img1)
+    # r2 = _generate_hist(img2)
+    return _sim_colour(r1, r2), _sim_texture(r1, r2)
+
+def _extract_regions(img):
+
+    R = {}
+
+    # get hsv image
+    hsv = skimage.color.rgb2hsv(img[:, :, :3])
+
+    # pass 1: count pixel positions
+    for y, i in enumerate(img):
+
+        for x, (r, g, b, l) in enumerate(i):
+
+            # initialize a new region
+            if l not in R:
+                R[l] = {
+                    "min_x": 0xffff, "min_y": 0xffff,
+                    "max_x": 0, "max_y": 0, "labels": [l]}
+
+            # bounding box
+            if R[l]["min_x"] > x:
+                R[l]["min_x"] = x
+            if R[l]["min_y"] > y:
+                R[l]["min_y"] = y
+            if R[l]["max_x"] < x:
+                R[l]["max_x"] = x
+            if R[l]["max_y"] < y:
+                R[l]["max_y"] = y
+
+    # pass 2: calculate texture gradient
+    tex_grad = _calc_texture_gradient(img)
+
+    # pass 3: calculate colour histogram of each region
+    for k, v in list(R.items()):
+
+        # colour histogram
+        masked_pixels = hsv[:, :, :][img[:, :, 3] == k]
+        R[k]["size"] = len(masked_pixels / 4)
+        R[k]["hist_c"] = _calc_colour_hist(masked_pixels)
+
+        # texture histogram
+        R[k]["hist_t"] = _calc_texture_hist(tex_grad[:, :][img[:, :, 3] == k])
+
+    return R
+
+
+
+
+def selective_search(
+        im_orig, scale=1.0, sigma=0.8, min_size=50):
+    '''Selective Search
+
+    Parameters
+    ----------
+        im_orig : ndarray
+            Input image
+        scale : int
+            Free parameter. Higher means larger clusters in felzenszwalb segmentation.
+        sigma : float
+            Width of Gaussian kernel for felzenszwalb segmentation.
+        min_size : int
+            Minimum component size for felzenszwalb segmentation.
+    Returns
+    -------
+        img : ndarray
+            image with region label
+            region label is stored in the 4th value of each pixel [r,g,b,(region)]
+        regions : array of dict
+            [
+                {
+                    'rect': (left, top, width, height),
+                    'labels': [...],
+                    'size': component_size
+                },
+                ...
+            ]
+    '''
+    assert im_orig.shape[2] == 3, "3ch image is expected"
+
+    # load image and get smallest regions
+    # region label is stored in the 4th value of each pixel [r,g,b,(region)]
+    img = _generate_segments(im_orig, scale, sigma, min_size)
+
+    if img is None:
+        return None, {}
+
+    imsize = img.shape[0] * img.shape[1]
+    R = _extract_regions(img)
+
+    # extract neighbouring information
+    neighbours = _extract_neighbours(R)
+
+    # calculate initial similarities
+    S = {}
+    for (ai, ar), (bi, br) in neighbours:
+        S[(ai, bi)] = _calc_sim(ar, br, imsize)
+
+    # hierarchal search
+    while S != {}:
+
+        # get highest similarity
+        i, j = sorted(S.items(), key=lambda i: i[1])[-1][0]
+
+        # merge corresponding regions
+        t = max(R.keys()) + 1.0
+        R[t] = _merge_regions(R[i], R[j])
+
+        # mark similarities for regions to be removed
+        key_to_delete = []
+        for k, v in list(S.items()):
+            if (i in k) or (j in k):
+                key_to_delete.append(k)
+
+        # remove old similarities of related regions
+        for k in key_to_delete:
+            del S[k]
+
+        # calculate similarity set with the new region
+        for k in [a for a in key_to_delete if a != (i, j)]:
+            n = k[1] if k[0] in (i, j) else k[0]
+            S[(t, n)] = _calc_sim(R[t], R[n], imsize)
+
+    regions = []
+    for k, r in list(R.items()):
+        regions.append({
+            'rect': (
+                r['min_x'], r['min_y'],
+                r['max_x'] - r['min_x'], r['max_y'] - r['min_y']),
+            'size': r['size'],
+            'labels': r['labels']
+        })
+
+    return img, regions
